e6ad61805e51d76c9768f8c18581c5e9a2c350f4
mehhovcki <145855444+mehh0vcki@users.noreply.github.com>
2025-02-10 16:19:42 +0300

    make icons visible

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const socialIcons = document.getElementById('socialIcons');
+    
+    setTimeout(() => {
+        socialIcons.classList.add('visible');
+    }, 2000);
+});
